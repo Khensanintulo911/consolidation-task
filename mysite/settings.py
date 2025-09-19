35fc1b77a13a88cfce9b05ea382fbb7adcdbc2d3
Khensani <danielntulo@gmail.com>
2025-09-19 03:36:56 +0200

    Updated Dockerfile and requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files
+    #'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files
 ]
 
 ROOT_URLCONF = 'mysite.urls'
